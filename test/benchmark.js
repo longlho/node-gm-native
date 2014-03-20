bc957d0444077875eb85b2e29172abbce69f846b
longlho <holevietlong@gmail.com>
2014-03-20 00:22:11 -0400

    some benchmark adjustments

--- test/benchmark.js
+++ test/benchmark.js
@@ -13,9 +13,14 @@ console.timeEnd('Native IM 100 times');
 console.time('Spawn 100 IM processes');
 var done = 100;
 for (i = 0; i < 100; i++) {
-  execFile("convert", ["http://radioedit.iheart.com/service/img/nop()/assets/images/1469.png", "-resize", "100x100", "webp:-"], {
-    timeout: 1000
+  execFile("convert", ["http://radioedit.iheart.com/service/img/nop()/assets/images/1469.png", "-resize", "100x100", '-background', 'transparent', '-gravity', 'North', '-extent', '100x100', "webp:-"], {
+    timeout: 2000
   }, function (err, stdout, stderr) {
+    if (err || !stdout || !stdout.length) {
+      console.log('err spawning convert');
+      console.log(err);
+      process.exit(1);
+    }
     done--;
     if (!done) {
       console.timeEnd('Spawn 100 IM processes');
@@ -55,7 +60,7 @@ function testAsyncReqExec() {
   var done = 100;
   for (var i = 0; i < 100; i++) {
     http.get("http://radioedit.iheart.com/service/img/nop()/assets/images/1469.png", function (resp) {
-      var ps = execFile("convert", ["-", "-resize", "100x100", "webp:-"], {
+      var ps = execFile("convert", ["-", "-resize", "100x100", '-background', 'transparent', '-gravity', 'North', '-extent', '100x100', "webp:-"], {
         timeout: 1000
       }, function (err, stdout, stderr) {
         done--;
